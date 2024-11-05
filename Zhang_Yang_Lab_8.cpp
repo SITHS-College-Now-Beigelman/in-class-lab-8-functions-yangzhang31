a6633a9d1789603624115ae0fcef0fe30cbd1b87
yangzhang31 <yangz15@nycstudents.net>
2024-11-05 11:00:45 -0500

    gi

--- Zhang_Yang_Lab_8.cpp
+++ Zhang_Yang_Lab_8.cpp
@@ -10,8 +10,10 @@ using namespace std;
 int doubleMultiply(int a, int b) { //the function named doubleMultiply with two things
     int product; //Declares varialbe
     product = a * b * 2; //that varialbe is equal to a * b
-    return product;
+    return product; //Returns that product
 }
+
+double getSum(double x, double y, double z);
 
 int main() {
     cout << endl;
@@ -37,11 +39,43 @@ int main() {
     int num1, num2; //The two int variables
     cout << "Enter two integers: "; //prompts user for the two integers
     cin >> num1 >> num2; //stores the two integers
-    cout << "Twice the product of the numbers is: " << doubleMultiply(num1, num2) << endl; //outputs the product of the numbers 
+    cout << "Twice the product of the numbers is: " << doubleMultiply(num1, num2) << endl; //outputs the product of the numbers using the fucntion created at the top 
 
     cout << endl;
     cout << "----------------- PART THREE -----------------" << endl;
     cout << endl;
 
+    double numOne, numTwo, numThree;
+    cout << "Enter three numbers seperated by spaces: ";
+    cin >> numOne >> numTwo >> numThree;
+    cout << "The sum of " << numOne << ", " << numTwo << ", and " << numThree << " is " << getSum(numOne, numTwo, numThree) << endl;
+
     return 0; //returns a 0 to the function main
 }
+
+double getSum(double x, double y, double z) {
+    double result; 
+    result = x + y +z; 
+    return result;
+}
+
+/*
+
+----------------- PART ONE -----------------
+
+Please input a number with decimals: 2.2
+Used function sqrt to find that the square root of 2.2 is 1.48324
+Used function pow to find that 2.2 to the power of the fourth is 23.4256
+Used function floor to find that the largest whole number that is not greater than 2.2 is 2
+
+----------------- PART TWO -----------------
+
+Enter two integers: 2 1
+Twice the product of the numbers is: 4
+
+----------------- PART THREE -----------------
+
+Enter three numbers seperated by spaces: 3 4 5
+The sum of 3, 4, and 5 is 12
+
+*/
